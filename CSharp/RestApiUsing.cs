998b15ad2233e7a179468d2b605deaf9cfa8e128
Omer Y. Bayraktar <70052500+BySuspect@users.noreply.github.com>
2023-05-23 21:22:34 +0300

    Update RestApiUsing.cs

--- CSharp/RestApiUsing.cs
+++ CSharp/RestApiUsing.cs
@@ -4,5 +4,5 @@ RestClient client = new RestClient("~URL~");
 RestRequest request = new RestRequest();
 request.AddHeader("Authorization", Token);
 request.AddHeader("Content-Type", "application/json");
-var resCariGrup = await client.ExecuteAsync(request, Method.Post);
-var dataCariGrup = JsonConvert.DeserializeObject<~ROOTCLASS~>(resCariGrup.Content);
+var res = await client.ExecuteAsync(request, Method.Post);
+var data = JsonConvert.DeserializeObject<~ROOTCLASS~>(res.Content);
