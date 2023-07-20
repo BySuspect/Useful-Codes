8727a59b0f06474c0b5340c7670b15034056cb7c
Omer Y. Bayraktar <omerbayraktar43@gmail.com>
2023-07-20 12:57:28 +0300

    Update WindowsStartupHelper.cs

--- CSharp/WindowsForms/WindowsStartupHelper.cs
+++ CSharp/WindowsForms/WindowsStartupHelper.cs
@@ -10,6 +10,8 @@ namespace MyProject
             using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
             {
                 key.SetValue("My Application", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MyApplication.exe"));
+                //key.SetValue(AppDomain.CurrentDomain.FriendlyName.Replace(".exe", ""), Path.Combine(AppDomain.CurrentDomain.BaseDirectory, AppDomain.CurrentDomain.FriendlyName));
+
             }
         }
 
@@ -18,6 +20,7 @@ namespace MyProject
             using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
             {
                 key.DeleteValue("My Application", false);
+                //key.DeleteValue(AppDomain.CurrentDomain.FriendlyName.Replace(".exe", ""), false);
             }
         }
     }
